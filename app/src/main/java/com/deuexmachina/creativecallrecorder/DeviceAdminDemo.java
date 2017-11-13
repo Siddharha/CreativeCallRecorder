b18b873402e7b7bc9032d465b75926c4f528183e
siddhartha <siddhartha.maji@bluehorse.in>
2017-11-13 18:37:39 +0530

    Initial commit

---
+++ app/src/main/java/com/deuexmachina/creativecallrecorder/DeviceAdminDemo.java
@@ -0,0 +1,22 @@
+package com.deuexmachina.creativecallrecorder;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by siddhartha on 13/11/17.
+ */
+
+public class DeviceAdminDemo  extends DeviceAdminReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+
+    public void onEnabled(Context context, Intent intent) {
+    }
+
+    public void onDisabled(Context context, Intent intent) {
+    }
+}
